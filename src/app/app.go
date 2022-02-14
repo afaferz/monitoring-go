1a0970f579228c9092b15b4950383d36ca4a3bab
afaferz <afa00.papa@gmail.com>
2022-02-14 02:55:48 -0300

    Add read JSON File

--- src/app/app.go
+++ src/app/app.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
 )
 
+const BASE_DIR = "src/app"
 const times_of_monitoring = 5
 const delay_between_monitoring = 5 // Seconds
 
 func main() {
 	introduction()
+	readJSONSites()
 	for {
 		showMenu()
 		command := getCommandSelected()
@@ -51,42 +55,16 @@ func getCommandSelected() int {
 	return command
 }
 
-type TSite struct {
-	url         string
-	environment string
-	project     string
+type Site struct {
+	Url         string `json:"url"`
+	Environment string `json:"environment"`
+	Project     string `json:"project"`
 }
 
 func initMonitore() {
 	fmt.Println("Starting monitoring...")
-	sites_to_monitore := []TSite{
-		{
-			url:         "https://staging-administrativo.mova.vc",
-			environment: "staging",
-			project:     "administrativo",
-		},
-		{
-			url:         "https://administrativo.mova.vc",
-			environment: "production",
-			project:     "operacional",
-		},
-		{
-			url:         "https://staging-operacional.mova.vc",
-			environment: "staging",
-			project:     "operacional",
-		},
-		{
-			url:         "https://operacional.mova.vc",
-			environment: "production",
-			project:     "operacional",
-		},
-		{
-			url:         "https://mova.vc",
-			environment: "production",
-			project:     "mova",
-		},
-	}
 
+	sites_to_monitore := readJSONSites()
 	for i := 0; i < times_of_monitoring; i++ {
 		fmt.Println("----------------------")
 		fmt.Println("Testing", i+1, "time(s)")
@@ -98,19 +76,44 @@ func initMonitore() {
 	}
 }
 
-func testSiteStatus(site TSite) {
-	response, _ := http.Get(site.url)
+func testSiteStatus(site Site) {
+	response, err := http.Get(site.Url)
+	if err != nil {
+		fmt.Println("An error occurred")
+		fmt.Println(err)
+	}
 	if response.StatusCode == 200 {
-		fmt.Println("SITE:", site.url)
-		fmt.Println("PROJECT:", site.project)
+		fmt.Println("SITE:", site.Url)
+		fmt.Println("PROJECT:", site.Project)
 		fmt.Println("STATUS", response.StatusCode)
-		fmt.Println("ENVIRONMENT:", site.environment)
+		fmt.Println("ENVIRONMENT:", site.Environment)
 		fmt.Println("Site is OK!")
 	} else {
-		fmt.Println("SITE:", site.url)
-		fmt.Println("PROJECT:", site.project)
+		fmt.Println("SITE:", site.Url)
+		fmt.Println("PROJECT:", site.Project)
 		fmt.Println("STATUS:", response.StatusCode)
-		fmt.Println("ENVIRONMENT:", site.environment)
+		fmt.Println("ENVIRONMENT:", site.Environment)
 		fmt.Println("Site", site, "have a problem :(")
 	}
+}
+
+func readJSONSites() []Site {
+	JSONFile := fmt.Sprintf("%s/sites-to-monitore.json", BASE_DIR)
+	// file, err := os.Open(JSONFile)
+	file, err := ioutil.ReadFile(JSONFile)
+	if err != nil {
+		fmt.Println("----------------------")
+		fmt.Println("An error in OPEN occurred")
+		fmt.Println(err)
+		fmt.Println("----------------------")
+	}
+	var parsedJSON []Site
+	err = json.Unmarshal(file, &parsedJSON)
+	if err != nil {
+		fmt.Println("----------------------")
+		fmt.Println("An error in CONVERT JSON occurred")
+		fmt.Println(err)
+		fmt.Println("----------------------")
+	}
+	return parsedJSON
 }
